02ea3feab125f4e7684914a56ea107a2600968bd
Alexandr <novikalexandr1@gmail.com>
2022-01-13 01:02:20 +0300

    Refactoring

--- go.mod
+++ go.mod
@@ -3,3 +3,4 @@ module terminal-reg-auth
 go 1.17
 
 require github.com/mattn/go-sqlite3 v1.14.9 // indirect
+
--- main.go
+++ main.go
@@ -1,82 +1,77 @@
 package main
 
+// TODO: Вынести логин и рег в отдельные пакеты
+
 import (
 	"database/sql"
 	"fmt"
-	"time"
+	"login/login"
+	"reg/reg"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
-type user struct {
-	username string
-	password string
-}
+// type user struct {
+// 	username string
+// 	password string
+// }
 
-func login(db *sql.DB) error {
-	var insertedLogin string
-	fmt.Printf("Enter your login\n> ")
-	fmt.Scanf("%s\n", &insertedLogin)
+// func login(db *sql.DB) error {
+// 	var insertedLogin string
+// 	fmt.Printf("Enter your login\n> ")
+// 	fmt.Scanf("%s\n", &insertedLogin)
 
-	var insertedPassword string
-	fmt.Printf("Enter your password\n> ")
-	fmt.Scanf("%s\n", &insertedPassword)
+// 	var insertedPassword string
+// 	fmt.Printf("Enter your password\n> ")
+// 	fmt.Scanf("%s\n", &insertedPassword)
 
-	// Connect to db.
-	db, err := sql.Open("sqlite3", "database.db")
-	if err != nil {
-		return err
-	}
-	err = db.Ping()
-	if err != nil {
-		return err
-	}
-	defer db.Close()
+// 	// Connect to db.
+// 	db, err := sql.Open("sqlite3", "database.db")
+// 	if err != nil {
+// 		return err
+// 	}
+// 	err = db.Ping()
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer db.Close()
 
-	// Check if user whith this credintials exists.
-	rows, err := db.Query("SELECT username, password FROM users WHERE username = ? AND password = ?;", insertedLogin, insertedPassword)
-	if err != nil {
-		return err
-	}
-	defer rows.Close()
-	user := user{}
-	for rows.Next() {
-		err := rows.Scan(&user.username, &user.password)
-		if err != nil {
-			return err
-		}
-	}
-	fmt.Printf("Hi: %s\nYour password: %s\nYou have successfully logged in.\n", user.username, user.password)
+// 	// Check if user whith this credintials exists.
+// 	rows, err := db.Query("SELECT username, password FROM users WHERE username = ? AND password = ?;", insertedLogin, insertedPassword)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer rows.Close()
+// 	user := user{}
+// 	for rows.Next() {
+// 		err := rows.Scan(&user.username, &user.password)
+// 		if err != nil {
+// 			return err
+// 		}
+// 	}
+// 	fmt.Printf("Hi: %s\nYour password: %s\nYou have successfully logged in.\n", user.username, user.password)
 
-	return nil
-}
+// 	return nil
+// }
 
-func reg(db *sql.DB) error {
-	var login string
-	fmt.Printf("Add you login\n> ")
-	fmt.Scanf("%s\n", &login)
+// func reg(db *sql.DB) error {
+// 	var login string
+// 	fmt.Printf("Add you login\n> ")
+// 	fmt.Scanf("%s\n", &login)
 
-	var pass string
-	fmt.Printf("Add you password\n> ")
-	fmt.Scanf("%s\n", &pass)
+// 	var pass string
+// 	fmt.Printf("Add you password\n> ")
+// 	fmt.Scanf("%s\n", &pass)
 
-	_, err := db.Exec("insert into users (username, password, reg_time) values ($1, $2, $3)",
-		login, pass, time.Now())
-	if err != nil {
-		panic(err)
-	}
+// 	_, err := db.Exec("insert into users (username, password, reg_time) values ($1, $2, $3)",
+// 		login, pass, time.Now())
+// 	if err != nil {
+// 		panic(err)
+// 	}
 
-	// file, err := os.Create(login + ".txt")
-	// if err != nil {
-	// 	return fmt.Errorf("Unable to create file:")
-	// }
-
-	// defer file.Close()
-	// file.WriteString(pass)
-
-	fmt.Printf("Hi, %s your registration complete\n", login)
-	return nil
-}
+// 	fmt.Printf("Hi, %s your registration complete\n", login)
+// 	return nil
+// }
 
 func main() {
 	db, err := sql.Open("sqlite3", "database.db")
@@ -92,7 +87,7 @@ func main() {
 	switch choose {
 	case "login":
 		for {
-			err := login(db)
+			err := login.Login(db)
 			if err != nil {
 				fmt.Println(err)
 				continue
@@ -102,7 +97,7 @@ func main() {
 
 	case "register":
 		for {
-			err := reg(db)
+			err := reg.Reg(db)
 			if err != nil {
 				fmt.Println(err)
 				continue
